cf40dffad084abb42660fb50260e03c1c82e85ed
Mizuki Yamada <58352421+sashiming@users.noreply.github.com>
2025-09-01 19:08:44 +0900

    [fwectf2025] change rev/No_need_Logical_Thinking/solve.py

--- fwectf2025/rev/No_need_Logical_Thinking/solve.py
+++ fwectf2025/rev/No_need_Logical_Thinking/solve.py
@@ -1,10 +1,6 @@
 with open('./output.txt', 'rb') as f:
-    data = f.read()
+    data = bytes([c for c in f.read() if c != 0xC2])
     original_data = b''
-    countc2 = 0
     for i in range(len(data)):
-        if data[i] == 0xC2:
-            countc2 += 1
-        else:
-            original_data += bytes([data[i] - (i+1 - countc2)])
+        original_data += bytes([data[i] - (i+1)])
     print(original_data)
